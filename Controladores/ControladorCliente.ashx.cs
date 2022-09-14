8a4640fb4e7888ebf8d70990dfa7f1c361cf7e78
miller ossa samboni <miller.ossa@sofka.com.co>
2022-09-14 14:40:42 -0500

    refactor se añade try y catch

--- Controladores/ControladorCliente.ashx.cs
+++ Controladores/ControladorCliente.ashx.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.IO;
+﻿using AppClase.Clases;
 using AppClase.Modelos;
 using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Web;
 
 namespace AppClase.Controladores
 {
@@ -13,16 +12,38 @@ namespace AppClase.Controladores
 
         public void ProcessRequest(HttpContext context)
         {
-            StreamReader reader = new StreamReader(context.Request.InputStream);
-            string DatosCliente = reader.ReadToEnd();
-            //Vamos a convertir el texto que está en la variable DatosCliente, a una clase: del modelo Cliente
-            //Se hace con las clases de Newtonsoft, utilizar la clase jsonconvert
-            Cliente _Cliente = JsonConvert.DeserializeObject<Cliente>(DatosCliente);
+            try
+            {
+                string DatosCliente;
+                StreamReader reader = new StreamReader(context.Request.InputStream);
+                 DatosCliente = reader.ReadToEnd();
+                //Vamos a convertir el texto que está en la variable DatosCliente, a una clase: del modelo Cliente
+                //Se hace con las clases de Newtonsoft, utilizar la clase jsonconvert
+                Cliente _Cliente = JsonConvert.DeserializeObject<Cliente>(DatosCliente);
+                context.Response.Write("Hello " + _Cliente.Nombre + " " + _Cliente.PrimerApellido + " " + _Cliente.SegundoApellido);
+            }
+            catch (Exception ex)
+            {
+                context.Response.Write(ex.ToString());
+            }
+           
+
 
            
-            context.Response.Write("Hello " +_Cliente.Nombre + " "+_Cliente.PrimerApellido + " "+_Cliente.SegundoApellido);
         }
-
+        private string Insertar(Cliente _cliente)
+        {
+            clsCliente oCliente = new clsCliente();
+            oCliente._cliente = _cliente;
+            if (oCliente.Insertar())
+            {
+                return "Se insertó en la base de datos";
+            }
+            else
+            {
+                return "";
+            }
+        }
         public bool IsReusable
         {
             get
